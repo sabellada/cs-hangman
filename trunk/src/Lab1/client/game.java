477d3d322700ef7f38676f966d205e2fdbc443bb
joe.oommen <joe.oommen@ce9ef95e-8652-11dd-a5c9-f1483cedc3eb>
2008-09-26 15:32:27 +0000

    Multiple games can run correctly.

--- trunk/src/Lab1/client/game.java
+++ trunk/src/Lab1/client/game.java
@@ -19,64 +19,56 @@ import java.io.*;
  * @author Jerome
  */
 public class game extends JPanel{
-    
-    gui myGameBoard;
-    /** Creates a new instance of game */
-    public game() {   
-        myGameBoard = new gui();        
-        // no need to modify the two following lines
-        // all they do is add the game board to the panel
-        setLayout(new BorderLayout());                
-        add(myGameBoard, BorderLayout.CENTER);
-    }
-    
-    /*
-     * Communication with the server
-     */
 
-    /*  LAB 1 : WRITE THE FOLLOWING CODE 
-     *          This client process will contact the server in order to get the word to guess
-     *          This method returns a String that should be send to gui.java
-     *          Refer to the method 'public void resetGameBoard()' in gui.java
-     */
-    public String startNewGame(){
-        String theNewWordToGuess="";
-        try{
-            
-        	//Socket ConnectionToServer=new Socket();
-        	//DataInputStream in = new DataInputStream(ConnectionToServer.getInputStream());
-        	//theNewWordToGuess=in.readUTF();
-        	
-        }catch(Exception e){
-            // if you are here, something went wrong when contacting the server
-            // it is likely that the process is not able to get the new word to guess
-        }
-                
-        return theNewWordToGuess;
-    }
-    
-    
-    // NO NEED TO MODIFY THE REMAINING OF THE CLASS
-    // ************************************************************************************
-    private static void createAndShowGUI() {        
-        JFrame frame = new JFrame("SYSC 4504 Lab");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        game thegame =new game();
-        frame.add(thegame, BorderLayout.CENTER);
-        frame.setPreferredSize(new Dimension(600,420));
-        frame.pack();
-        frame.setVisible(true);
-      
-        
-    }   
-    public static void main(String[] args) {
-    	SwingUtilities.invokeLater(new Runnable() {
-    		public void run() {                
-    			UIManager.put("swing.boldMetal", Boolean.FALSE);
-    			createAndShowGUI();
-    		}
-    	});         
-    }      
-    // ************************************************************************************
+	private gui myGameBoard;
+	private int roundsPlayed=0;
+	/** Creates a new instance of game */
+	public game() {   
+		myGameBoard = new gui();        
+		// no need to modify the two following lines
+		// all they do is add the game board to the panel
+		setLayout(new BorderLayout());                
+		add(myGameBoard, BorderLayout.CENTER);
+	}
+
+	/*
+	 * Communication with the server
+	 */
+
+	/*  LAB 1 : WRITE THE FOLLOWING CODE 
+	 *          This client process will contact the server in order to get the word to guess
+	 *          This method returns a String that should be send to gui.java
+	 *          Refer to the method 'public void resetGameBoard()' in gui.java
+	 */
+	public  String startNewGame(){
+		String theNewWordToGuess="";      
+
+		return theNewWordToGuess;
+	}
+
+
+	// NO NEED TO MODIFY THE REMAINING OF THE CLASS
+	// ************************************************************************************
+	private static void createAndShowGUI() {        
+		JFrame frame = new JFrame("SYSC 4504 Lab");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		game thegame =new game();
+		frame.add(thegame, BorderLayout.CENTER);
+		frame.setPreferredSize(new Dimension(600,420));
+		frame.pack();
+		frame.setVisible(true);
+
+
+	}   
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {                
+				UIManager.put("swing.boldMetal", Boolean.FALSE);
+				createAndShowGUI();
+			}
+		});       
+
+	}      
+	// ************************************************************************************
 }
