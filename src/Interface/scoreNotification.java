0d5f6983d1809b0431f9ac76833728f3cae07a16
Riley.Onabigon <Riley.Onabigon@ce9ef95e-8652-11dd-a5c9-f1483cedc3eb>
2008-10-24 08:01:40 +0000

    scoreNotification interface

---
+++ src/Interface/scoreNotification.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Lab2.Interface;
+
+/**
+ *
+ * @author Jerome
+ */
+public interface scoreNotification extends java.rmi.Remote {
+    void notify(String player, int score)throws java.rmi.RemoteException; 
+}
