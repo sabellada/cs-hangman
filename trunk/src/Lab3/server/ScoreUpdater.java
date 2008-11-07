964134f80d6c1fbf7fdae45263d63382d9c53d8f
joe.oommen <joe.oommen@ce9ef95e-8652-11dd-a5c9-f1483cedc3eb>
2008-11-07 04:45:37 +0000

    Lab3 - server

---
+++ trunk/src/Lab3/server/ScoreUpdater.java
@@ -0,0 +1,29 @@
+package Lab2.server;
+
+import java.rmi.RemoteException;
+
+import Lab2.Interface.scoreNotification;
+
+public class ScoreUpdater implements scoreNotification{
+
+	ScoreMonitor scoreMonitor;
+	
+	public ScoreUpdater(ScoreMonitor scoreMonitor){
+		this.scoreMonitor=scoreMonitor;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see Lab2.Interface.scoreNotification#notify(java.lang.String, int)
+	 */
+	@Override
+	public void notify(String player, int score) throws RemoteException {
+		scoreMonitor.addScore(player, score);
+	}
+
+	//placeholder method to get scores, will be updated in lab 3
+	@Override
+	public String scores() {
+		return scoreMonitor.getScores();
+	}
+
+}
